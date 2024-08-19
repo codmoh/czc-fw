bcea3b6f9380595aa87c5beba48f4c92de957fc7
Sean Bikkes <bixkitts@gmail.com>
2024-08-19 20:41:39 +0200

    our version of functions with my patches

--- src/websrc/js/functions.js
+++ src/websrc/js/functions.js
@@ -2048,6 +2048,9 @@ function WifiEnbl(state) {
 	$("#wifiPwr").prop(disbl, state);
 	if (dhcpEnabled) {
 		$("#wifiDhcp").prop(disbl, state);
+		$("#wifiIp").prop(disbl, true);
+		$("#wifiMask").prop(disbl, true);
+		$("#wifiGate").prop(disbl, true);
 		$("#wifiDns1").prop(disbl, true);
 		$("#wifiDns2").prop(disbl, true);
 	} else {
@@ -2146,37 +2149,6 @@ function logRefresh(ms) {
 		});
 	}, ms);
 }
-
-// CSS class name for dark theme
-const darkTheme = "dark-theme";
-
-// Add dark theme change here
-const darkThemeSetUp = () => {
-	if (getCurrentTheme() === "dark") {
-		document.getElementById("toggleBtn").checked = true;
-	} else {
-		document.getElementById("toggleBtn").checked = false;
-	}
-};
-
-const getCurrentTheme = () =>
-	document.body.classList.contains(darkTheme) ? "dark" : "light";
-
-//   Get user's theme preference from local storage
-const selectedTheme = localStorage.getItem("selected-theme");
-if (selectedTheme === "dark") {
-	document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-		darkTheme
-	);
-	darkThemeSetUp();
-}
-
-const themeButton = document.getElementById("toggleBtn");
-themeButton.addEventListener("change", () => {
-	document.body.classList.toggle(darkTheme);
-	localStorage.setItem("selected-theme", getCurrentTheme());
-	darkThemeSetUp();
-});
 
 let languages = [
 	{ value: "en", text: "🇬🇧 English" },
