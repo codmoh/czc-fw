eccaf93bcf71e23b8db3dd514a0b12494305ee9e
ole <hofmann.ole.eibach@gmail.com>
2025-06-06 09:48:55 +0200

    changed versioning

--- tools/build/build.py
+++ tools/build/build.py
@@ -2,7 +2,7 @@
 
 Import("env")
 
-from subprocess import call
+import subprocess
 import shutil
 import os
 import time
@@ -15,16 +15,11 @@ from func import print_colored
 
 VERSION_HEADER = "version.h"
 
-def extract_version_from_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                if '#define VERSION' in line:
-                    version = line.split('"')[1]
-                    return version
-    except FileNotFoundError:
-        print("File not found")
-    return None
+
+def get_last_git_tag():
+    command = ['git', 'describe', '--exact-match', '--tags']
+    tmp = subprocess.check_output(command)
+    return tmp[1:]
 
 def after_build(source, target, env):
     time.sleep(2)
@@ -39,7 +34,7 @@ def after_build(source, target, env):
 
     VERSION_FILE = "src/" + VERSION_HEADER
     
-    VERSION_NUMBER = extract_version_from_file(VERSION_FILE)
+    VERSION_NUMBER = get_last_git_tag()
     
     NEW_NAME_BASE = "bin/czc_fw_" + VERSION_NUMBER
     
@@ -65,3 +60,4 @@ def after_build(source, target, env):
 env.AddPostAction("buildprog", after_build)
 
 firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
+
